c02776ce512cfb0721ea29a52b15a23592688ac5
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-29 23:27:13 +0900

    day 40

---
+++ Baekjoon/stage/17/5086.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	while (a || b) {
+		if (b % a == 0) cout << "factor" << "\n";
+		else if (a % b == 0) cout << "multiple" << "\n";
+		else cout << "neither" << "\n";
+		cin >> a >> b;
+	}
+	return 0;
+}
