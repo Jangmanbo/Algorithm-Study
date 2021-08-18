1f58a45f10f5a3588b073f5609f60bc95e9cbd25
Jangmanbo <wkddpdnjs99@gmail.com>
2021-08-18 23:35:05 +0900

    day 46

---
+++ Baekjoon/stage/17/1037.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, num, min = 1000000, max = 2;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		if (num < min) min = num;
+		if (num > max) max = num;
+	}
+	cout << min * max;
+}
