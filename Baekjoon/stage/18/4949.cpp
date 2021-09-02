fec610d0b4698270dac710ddb37c7c61dde96376
Jangmanbo <wkddpdnjs99@gmail.com>
2021-09-02 23:20:52 +0900

    day 50

---
+++ Baekjoon/stage/18/4949.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	int size;
+	string str;
+	bool VPS = true;
+	stack<char> stk;
+	while (getline(cin, str)) {
+		if (str == ".") break;
+		size = str.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (str[i] == '(' || str[i] == '[') stk.push(str[i]);
+			else if (str[i] == ')') {
+				if (!stk.empty() && stk.top() == '(') {
+					stk.pop();
+				}
+				else {
+					VPS = false;
+					break;
+				}
+			}
+			else if (str[i] == ']') {
+				if (!stk.empty() && stk.top() == '[') {
+					stk.pop();
+				}
+				else {
+					VPS = false;
+					break;
+				}
+			}
+		}
+		//결과 출력
+		if (stk.empty() && VPS) cout << "yes" << "\n";
+		else cout << "no" << "\n";
+		//초기화
+		VPS = true;
+		stk = stack<char>();
+	}
+}
+
