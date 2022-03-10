904312e96e5972c213353c0e2cdf79d87b6ead17
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-10 17:32:26 +0900

    day 72

--- Baekjoon/11404.cpp
+++ Baekjoon/11404.cpp
@@ -2,68 +2,50 @@
 #include <algorithm>
 using namespace std;
 
-int map[101][101];	// 버스 비용
-bool visit[101][101];	// 버스 사용 체크
-int ans[101][101];
-int N, M, a, b, c;
-
-void dfs(int start, int def, int cost)
-{
-	// 적은 비용만 기록
-	if (ans[start][def])
-		ans[start][def] = min(ans[start][def], cost);
-	else
-		ans[start][def] = cost;
-
-	for (int j = 1; j <= N; j++)
-	{
-		// def를 경유지로 하는 노선 탐색
-		if (start != j && map[def][j] && !visit[def][j])
-		{
-			visit[def][j] = true;
-			dfs(start, j, cost + map[def][j]);
-			visit[def][j] = false;
-		}
-	}
-}
+#define INF 9999999
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	
+	int map[101][101];
+	int N, M, a, b, c;
+
 	cin >> N;
 	cin >> M;
-
-	while (M--)
-	{
-		cin >> a >> b >> c;
-		// 비용이 적은 버스만 사용
-		if (map[a][b])
-			map[a][b] = ans[a][b] = min(map[a][b], c);
-		else
-			map[a][b] = ans[a][b] = c;
-	}
 
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 1; j <= N; j++)
 		{
-			// i->j 버스가 있음
-			if (i != j && map[i][j])
-			{
-				visit[i][j] = true;
-				dfs(i, j, map[i][j]);
-				visit[i][j] = false;
-			}
+			if (i != j)
+				map[i][j] = INF;
+			else
+				map[i][j] = 0;
 		}
 	}
+
+	while (M--)
+	{
+		cin >> a >> b >> c;
+		map[a][b] = min(map[a][b], c);	// 비용이 적은 버스만 사용
+	}
+
+	for (int k = 1; k <= N; k++)
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+				map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
 
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 1; j <= N; j++)
-			cout << ans[i][j] << ' ';
+		{
+			if (map[i][j] != INF)
+				cout << map[i][j] << ' ';
+			else
+				cout << 0 << ' ';
+		}
 		cout << '\n';
 	}
 }
