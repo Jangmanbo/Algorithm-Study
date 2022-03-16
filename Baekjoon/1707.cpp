b4bb2d0bbd02d51b4fc6064060bd362d370792fb
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-16 23:56:51 +0900

    day 74

--- Baekjoon/1707.cpp
+++ Baekjoon/1707.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
+#include <map>
 #include <vector>
 using namespace std;
 
 int K, V, E;
-vector<pair<int, int>> vertex;
 int color[20001];
+vector<int> graph[20001];
 
-void check()
+bool dfs(int key)
 {
-	int u, v;
-	for (int i = 0; i < E; i++)
-	{
-		u = vertex[i].first;
-		v = vertex[i].second;
+	// 인접한 정점이 없음
+	if (graph[key].size() == 0)
+		return true;
 
+	for (int i = 0; i < graph[key].size(); i++)
+	{
 		// 두 정점 모두 처음 체크
-		if (!color[u] && !color[v])
+		if (!color[key] && !color[graph[key][i]])
 		{
-			color[u] = 1;
-			color[v] = -1;
+			color[key] = 1;
+			color[graph[key][i]] = -1;
+			return dfs(graph[key][i]);
 		}
-		// 두 정점 모두 이미 체크했으며 두 색이 동일
-		else if (color[u] && color[v] && color[u] == color[v])
+		// 두 정점 모두 이전에 체크
+		else if (color[key] && color[graph[key][i]])
 		{
-			cout << "NO\n";
-			return;
+			if (color[key] == color[graph[key][i]])
+				return false;
 		}
 		else
 		{
-			if (color[u])
-				color[v] = -color[u];
+			// key만 이전에 체크
+			if (color[key])
+			{
+				color[graph[key][i]] = -color[key];
+				return dfs(graph[key][i]);
+			}
+			// vertex[i]만 이전에 체크
 			else
-				color[u] = -color[v];
+				color[key] = -color[graph[key][i]];
 		}
 	}
-	cout << "YES\n";
+	return true;
 }
 
 int main()
@@ -43,22 +50,39 @@ int main()
 	cin.tie(0);
 
 	int u, v;
+	bool ans = true;
 	cin >> K;
 
 	while (K--)
 	{
 		cin >> V >> E;
+
 		for (int i = 0; i < E; i++)
 		{
 			cin >> u >> v;
-			vertex.push_back({ u, v });
+
+			graph[u].push_back(v);
+			graph[v].push_back(u);
 		}
 
-		check();
+		for (int i = 1; i <= V; i++)
+		{
+			if (!dfs(i))
+			{
+				ans = false;
+				cout << "NO\n";
+				break;
+			}
+		}
+		if (ans)
+			cout << "YES\n";
 
 		// 초기화
-		vertex.clear();
 		for (int i = 1; i <= V; i++)
+		{
+			graph[i].clear();
 			color[i] = 0;
+		}
+		ans = true;
 	}
 }
