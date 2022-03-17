e165436bcaf56843cc8a6d832571dbd4f8ea7f55
Jangmanbo <wkddpdnjs99@gmail.com>
2022-03-17 23:56:03 +0900

    day 74

--- Baekjoon/1707.cpp
+++ Baekjoon/1707.cpp
@@ -12,77 +12,79 @@ bool dfs(int key)
 	// 인접한 정점이 없음
 	if (graph[key].size() == 0)
 		return true;
-
-	for (int i = 0; i < graph[key].size(); i++)
+	
+	for (int nxt : graph[key])
 	{
 		// 두 정점 모두 처음 체크
-		if (!color[key] && !color[graph[key][i]])
+		if (!color[key] && !color[nxt])
 		{
 			color[key] = 1;
-			color[graph[key][i]] = -1;
-			return dfs(graph[key][i]);
+			color[nxt] = -1;
+			return dfs(nxt);
 		}
 		// 두 정점 모두 이전에 체크
-		else if (color[key] && color[graph[key][i]])
+		else if (color[key] && color[nxt])
 		{
-			if (color[key] == color[graph[key][i]])
+			if (color[key] == color[nxt])
 				return false;
-		}
-		else
+		}// key만 이전에 체크
+		else if (color[key])
 		{
-			// key만 이전에 체크
-			if (color[key])
-			{
-				color[graph[key][i]] = -color[key];
-				return dfs(graph[key][i]);
-			}
-			// vertex[i]만 이전에 체크
-			else
-				color[key] = -color[graph[key][i]];
+			color[nxt] = -color[key];
+			return dfs(nxt);
 		}
+		// nxt만 이전에 체크
+		else
+			color[key] = -color[nxt];
 	}
 	return true;
 }
 
+bool solve() {
+	int u, v;
+	cin >> V >> E;
+
+	// 입력
+	for (int i = 0; i < E; i++)
+	{
+		cin >> u >> v;
+
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+
+	for (int i = 1; i <= V; i++)
+	{
+		if (!dfs(i))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void init() {
+	for (int i = 1; i <= V; i++)
+	{
+		graph[i].clear();
+		color[i] = 0;
+	}
+}
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int u, v;
-	bool ans = true;
 	cin >> K;
 
 	while (K--)
 	{
-		cin >> V >> E;
-
-		for (int i = 0; i < E; i++)
-		{
-			cin >> u >> v;
-
-			graph[u].push_back(v);
-			graph[v].push_back(u);
-		}
-
-		for (int i = 1; i <= V; i++)
-		{
-			if (!dfs(i))
-			{
-				ans = false;
-				cout << "NO\n";
-				break;
-			}
-		}
-		if (ans)
+		if (solve())
 			cout << "YES\n";
-
-		// 초기화
-		for (int i = 1; i <= V; i++)
-		{
-			graph[i].clear();
-			color[i] = 0;
-		}
-		ans = true;
+		else
+			cout << "NO\n";
+		init();
 	}
 }
