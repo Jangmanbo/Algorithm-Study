bbae6ed5d24bbb13facef2843c2fef72d9156cad
Jangmanbo <wkddpdnjs99@gmail.com>
2022-05-08 23:56:50 +0900

    day 85

--- Baekjoon/2156.cpp
+++ Baekjoon/2156.cpp
@@ -17,9 +17,9 @@ int main()
 	for (int i = 1; i < n; i++)
 	{
 		cin >> arr[i];
-		dp[i][0] = max(max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2] );
-		dp[i][1] = dp[i - 1][0] + arr[i];
-		dp[i][2] = dp[i - 1][1] + arr[i];
+		dp[i][0] = max(max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2] );	// 직전까지의 최대 포도주 양
+		dp[i][1] = dp[i - 1][0] + arr[i];	// 직전 포도주를 마시지 않은 경우
+		dp[i][2] = dp[i - 1][1] + arr[i];	// 직전에도, 지금도 마시는 경우
 	}
 
 	cout << max(max( dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2] );
